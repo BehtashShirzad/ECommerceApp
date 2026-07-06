2504a4905bc88c476f8ba3069d7a59fdab6e8fea
Mohamad <Mohamadshirzad81@gmail.com>
2026-07-07 00:45:29 +0330

    fx

--- src/ECommerce.Api/Program.cs
+++ src/ECommerce.Api/Program.cs
@@ -41,7 +41,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    await db.Database.EnsureCreatedAsync();
+     
     await db.Database.MigrateAsync();
     await DatabaseSeeder.SeedAsync(scope.ServiceProvider);
 }
